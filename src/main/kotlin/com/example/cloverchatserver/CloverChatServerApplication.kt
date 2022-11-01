7e752c2bc98ffdd8318d9a354470c031b493e8c7
Jongho Kim <zc798546@gmail.com>
2022-11-02 04:07:43 +0900

    first commit

---
+++ src/main/kotlin/com/example/cloverchatserver/CloverChatServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cloverchatserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloverChatServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloverChatServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cloverchatserver/CloverChatServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cloverchatserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloverChatServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
