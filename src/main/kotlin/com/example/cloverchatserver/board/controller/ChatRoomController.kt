f0e93c07756b46521d43fb48bfb0eddec787ef95
Jongho Kim <zc798546@gmail.com>
2022-11-02 23:36:02 +0900

    Change ChatRoomController request url

--- src/main/kotlin/com/example/cloverchatserver/board/controller/ChatRoomController.kt
+++ src/main/kotlin/com/example/cloverchatserver/board/controller/ChatRoomController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/board")
 class ChatRoomController(
     val chatRoomService: ChatRoomService
 ) {
